a96b991eb0bc95164e678121e487323a32fb2391
Tahar Mohamed Belkheir <mbelkhei@stevens.edu>
2018-02-25 18:12:49 -0500

    Update TF-IDF.py

--- Spark/TF-IDF.py
+++ Spark/TF-IDF.py
@@ -2,7 +2,7 @@ from pyspark import SparkConf, SparkContext
 from pyspark.mllib.feature import HashingTF
 from pyspark.mllib.feature import IDF
 
-# Initialize Spark
+# Create a SparkSession:
 conf = SparkConf().setMaster("local").setAppName("TFIDF")
 sc = SparkContext(conf = conf)
 
@@ -26,13 +26,11 @@ tfidf = idf.transform(tf)
 
 # the article for "Abraham Lincoln" is in the data, let's search for "Gettysburg" (Lincoln gave a famous speech there):
 
-# First, find out what hash value "Gettysburg" maps to by finding the
-# index a sparse vector from HashingTF gives us back:
+# First, find out what hash value "Gettysburg" maps to by finding the index a sparse vector from HashingTF gives us back:
 gettysburgTF = hashingTF.transform(["Gettysburg"])
 gettysburgHashValue = int(gettysburgTF.indices[0])
 
-# extract the TF*IDF score for Gettsyburg's hash value into
-# a new RDD for each document: 
+# extract the TF*IDF score for Gettsyburg's hash value into a new RDD for each document: 
 gettysburgRelevance = tfidf.map(lambda x: x[gettysburgHashValue])
 
 # zip in the document names:
